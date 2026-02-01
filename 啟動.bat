c27ccfdeef0a6cf616c8cef6c7f371e5a3cc993c
r381893 <focus36473647@gmail.com>
2026-02-01 13:05:57 +0800

    feat: auto copy path on local button click

--- 啟動.bat
+++ 啟動.bat
@@ -1,5 +1,31 @@
 @echo off
-cd /d "%~dp0"
-echo Starting Portfolio...
-start index.html
-exit
+chcp 65001 >nul
+echo =========================================
+echo   正在啟動 Portfolio 專案集...
+echo =========================================
+echo.
+
+set "TARGET_URL=%~dp0index.html"
+
+:: 1. 嘗試使用 Google Chrome
+if exist "C:\Program Files\Google\Chrome\Application\chrome.exe" (
+    echo [系統] 偵測到 Google Chrome，正在開啟...
+    start "" "C:\Program Files\Google\Chrome\Application\chrome.exe" "%TARGET_URL%"
+    goto :EOF
+)
+if exist "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" (
+    echo [系統] 偵測到 Google Chrome (x86)，正在開啟...
+    start "" "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" "%TARGET_URL%"
+    goto :EOF
+)
+
+:: 2. 嘗試使用 Microsoft Edge
+if exist "C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe" (
+    echo [系統] 偵測到 Microsoft Edge，正在開啟...
+    start "" "C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe" "%TARGET_URL%"
+    goto :EOF
+)
+
+:: 3. 如果都找不到，使用系統預設
+echo [系統] 未偵測到指定瀏覽器，使用系統預設開啟...
+start "" "%TARGET_URL%"
